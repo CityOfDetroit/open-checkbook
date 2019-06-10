83945530e801952e5ee49d35ecc85821023ba1fc
Jimmy McBroom <james.mcbroom@gmail.com>
2019-06-10 13:48:33 -0400

    clean up vendor-page

--- src/templates/vendor-page.js
+++ src/templates/vendor-page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql} from "gatsby";
-import { Header, Grid, Segment, List, Container } from 'semantic-ui-react';
+import { Header, Grid, List, Container } from 'semantic-ui-react';
 
 import Layout from '../components/layout';
 
@@ -14,17 +14,6 @@ export default ({ data }) => {
           {v.vendorName}
         </Header>
       </Grid.Row>
-
-      {/* <Grid.Row>
-        <Segment.Group compact size='big'>
-          <Segment>
-            <strong>{a.accountsPayablesByAgencyCode.totalCount.toLocaleString()}</strong> payments made to vendors in FY17-18
-          </Segment>
-          <Segment>
-            <strong>{formatted}</strong> total spent on goods & services
-          </Segment>
-        </Segment.Group>
-      </Grid.Row> */}
 
       <Grid.Row>
         <Container>
@@ -40,27 +29,6 @@ export default ({ data }) => {
           </List>
         </Container>
       </Grid.Row>
-
-      {/* <Grid.Row>
-        <Container>
-          <Header as='h3'>
-            Most recent payments
-          </Header>
-          <List divided relaxed>
-            {data.postgres.mostRecentPayments[0].accountsPayablesByAgencyCodeList.map((r, j) => (
-              <List.Item key={j}>
-                ${r.invoicePaymentDistAmount} to {r.vendorName} on {r.checkDate} for {r.objectDesc}
-              </List.Item>
-            ))}
-          </List>
-        </Container>
-      </Grid.Row> */}
-
-      {/* <Grid.Row>
-        <Header as='h3'>
-          ALL PAYMENTS TABLE TBD
-        </Header>
-      </Grid.Row> */}
 
     </Layout>
   );
