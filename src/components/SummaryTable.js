522559252cce33e9a875b618a20813ada6903e2b
Jimmy McBroom <james.mcbroom@gmail.com>
2019-06-12 22:30:16 -0400

    dynamic structured table :chocolate_bar:

--- src/components/SummaryTable.js
+++ src/components/SummaryTable.js
@@ -4,17 +4,19 @@ import { Table } from "semantic-ui-react";
 import Helpers from "../helpers";
 
 const SummaryTable = ({ tableData }) => {
+
+  console.log(tableData)
   
   // WIP: how do we dynamically make a semantic ui structured table where vendors can have rowSpan > 1 ???
   // see https://react.semantic-ui.com/collections/table/#types-structured
-  let uniqVendors = Object.keys(tableData);
-  let totalRows = 0;
-  let rowSpans = [];
+  // let uniqVendors = Object.keys(tableData);
+  // let totalRows = 0;
+  // let rowSpans = [];
 
-  for (let u of uniqVendors) {
-    totalRows += (Object.keys(tableData[u])).length;
-    rowSpans.push((Object.keys(tableData[u])).length)
-  }
+  // for (let u of uniqVendors) {
+  //   totalRows += (Object.keys(tableData[u])).length;
+  //   rowSpans.push((Object.keys(tableData[u])).length)
+  // }
   
   return (
     <Table>
@@ -29,7 +31,23 @@ const SummaryTable = ({ tableData }) => {
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {Array(totalRows).fill().map((_, i) => (
+        {Object.keys(tableData).map((t, i) => (
+          <>
+            {Object.keys(tableData[t]).map((c, j) => (
+              <Table.Row>
+                {j === 0 ? <Table.Cell rowSpan={Object.keys(tableData[t]).length} content={t} /> : null}
+                <Table.Cell content={tableData[t][c].reduce((a, p) => { return a + p.amount }, 0)} />
+                <Table.Cell content={c.split('-')[0]} />
+                <Table.Cell content={c.split('-')[1]} />
+                <Table.Cell content={c.split('-')[2]} />
+                <Table.Cell content={c.split('-')[3]} />
+
+              </Table.Row>
+            ))
+            }  
+          </>
+        ))}
+        {/* {Array(totalRows).fill().map((_, i) => (
           <Table.Row key={i}>
             <Table.Cell rowSpan={rowSpans[i]}>{Object.keys(tableData)[i]}</Table.Cell>
             <Table.Cell rowSpan='1'>${i}</Table.Cell>
@@ -38,7 +56,7 @@ const SummaryTable = ({ tableData }) => {
             <Table.Cell rowSpan='1'>exp cat</Table.Cell>
             <Table.Cell rowSpan='1'>exp</Table.Cell>
           </Table.Row>
-        ))}
+        ))} */}
       </Table.Body>
     </Table>
   );
