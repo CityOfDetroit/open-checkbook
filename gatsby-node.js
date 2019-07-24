2621349816cfd701c56da6eff7a8fab55dd9098b
jessica <jessica.mcinchak@gmail.com>
2019-07-24 09:28:18 -0400

    fix that bug, firstshould be outside of condition

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,7 +17,7 @@ exports.createPages = async ({ graphql, actions: { createPage }}) => {
           deptNameAbbreviation
           deptSlug
         }
-        vendors: allVendorsList(condition: {showInStats: true, first: 1}) {
+        vendors: allVendorsList(condition: {showInStats: true}) {
           vendorName
           vendorNumber
         }
